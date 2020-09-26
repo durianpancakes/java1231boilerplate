1cef63dbdd1a2cd082bd6af9a8e7039d458cc116
chanx <chanxuhui@outlook.com>
2020-09-26 12:23:58 +0800

    Complete SaladSpree code

--- src/main/java/codeit/template/resource/SaladResource.java
+++ src/main/java/codeit/template/resource/SaladResource.java
@@ -7,17 +7,60 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.json.*;
 
 import java.util.Map;
 
 @RestController
 public class SaladResource {
-    Logger logger = LoggerFactory.getLogger(SaladResource.class);
-
    @RequestMapping(value = "/salad-spree",method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
-    public Integer calculateSquare(@RequestBody Map<String, String> body){
-       Integer value = Integer.parseInt(body.get("number_of_salads"));
-       Integer value2 = Integer.parseInt(body.get("salad_prices_street_map"));
-       return value* value2;
+    public Integer saladSpree(@RequestBody Map<String, Object> body) throws JSONException {
+       Integer numSalads = Integer.parseInt(body.get("number_of_salads").toString());
+       boolean changed = false;
+       int price = Integer.MAX_VALUE;
+       String jsonString = body.get("salad_prices_street_map").toString();
+       JSONArray array = new JSONArray(jsonString);
+       String[][] map = new String[array.length()][];
+
+       for(int i = 0; i < array.length(); i++) {
+           String[] x_coor = array.get(i).toString().replace("\"", "")
+                   .replace("[", "")
+                   .replace("]", "")
+                   .split(",");
+           map[i] = x_coor;
+       }
+
+       for (int i = 0; i < map.length; i++) {
+           String[] currMap = map[i];
+
+           for (int j = 0; j < currMap.length - numSalads; j++) {
+               boolean hasX = false;
+               int tempPrice = 0;
+
+               for (int k = j; k < j + numSalads; k++) {
+                   if (currMap[k].equals("X")) {
+                       hasX = true;
+                       j = j + k;
+                       break;
+                   } else {
+                       int saladPrice = Integer.parseInt(currMap[k]);
+                       tempPrice += saladPrice;
+                   }
+               }
+
+               if (!hasX) {
+                   if (tempPrice < price) {
+                       price = tempPrice;
+                       changed = true;
+                   }
+               }
+           }
+       }
+
+       if (changed) {
+           return price;
+       } else {
+           return 0;
+       }
     }
 }
